492317ee8934d0553ebeffd47c48230aaa85a7d9
manserdaniel <daniel-manser@hotmail.com>
2020-06-04 08:28:13 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        string name, home, job;
+
+        public int Age { get; set; }
+
+        public Person(string name, string home, int age)
+        {
+            this.Name = name;
+            this.Home = home;
+            this.Age = age;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Home { get => home; set => home = value; }
+        public string Job { get => job; set => job = value; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFileContent = System.IO.File.ReadAllLines("C:\\Users\\daniel\\Source\\Repos\\ConsoleApp1\\ConsoleApp1\\persons.txt");
+            List<Person> persons = new List<Person>();
+           
+            foreach (string person in myFileContent)
+            {
+                string[] personSplitted = person.Split(";");
+                int.TryParse(personSplitted[2], out var age);
+                persons.Add(new Person(personSplitted[0], personSplitted[1], age));
+
+            }
+
+            persons.Find(p => p.Name == "Hans").Job = "Coder";
+            persons.Find(p => p.Name == "Helga").Job = "Coder";
+            persons.Find(p => p.Name == "Fritz").Job = "Officer";
+            persons.Find(p => p.Name == "Frida").Job = "Officer";
+
+            string[] myFileContentOutput = new string[persons.Count];
+
+            for(int i = 0; i < persons.Count; i++)
+            {
+                myFileContentOutput[i] = persons[i].Name + ";" + persons[i].Age + ";" + persons[i].Home + ";" + persons[i].Job;
+            }
+
+            System.IO.File.WriteAllLines("C:\\Users\\daniel\\Source\\Repos\\ConsoleApp1\\ConsoleApp1\\output.txt", myFileContentOutput);
+        }
+    }
+}
